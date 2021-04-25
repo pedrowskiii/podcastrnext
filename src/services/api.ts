2562a6f4fb87f49f41e937b014370289b235325c
Pedro Henrique <pedrohenrique.troncha@gmail.com>
2021-04-25 09:56:42 -0300

    alteração
    change package.jason

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,11 @@
-import axios from 'axios'
+// import axios from 'axios'
+
+// export const api = axios.create({
+//   baseURL:'http://localhost:3333/'
+// })
+
+import axios from 'axios';
 
 export const api = axios.create({
-  baseURL:'http://localhost:3333/'
+  baseURL: process.env.API_URL || 'http://localhost:3333/'
 })
