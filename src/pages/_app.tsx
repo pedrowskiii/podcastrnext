fe31ed433b37379fd397d28fa206acaf2f18bcc9
Pedro Henrique <pedrohenrique.troncha@gmail.com>
2021-04-25 07:04:22 -0300

    subindo projeto completo para o repositório
    primeiro commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,28 @@
+import '../styles/global.scss'
+
+import Header from '../components/Header'
+import Player from '../components/Player/index'
+
+
+
+import styles from '../styles/app.module.scss'
+import { PlayerContextProvider } from '../context/PlayerContext'
+
+function MyApp({ Component, pageProps }) {
+
+  return (
+    <PlayerContextProvider>
+      <div className={styles.wrapper}>
+        <main>
+
+          <Header />
+          <Component {...pageProps} />
+
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>
+  )
+}
+
+export default MyApp
